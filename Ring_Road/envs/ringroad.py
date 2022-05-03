63013d5f3608c5f4e039925ec134b40e9bf0676b
Vamsi995 <saivamsi.ds123@gmail.com>
2022-05-03 06:55:18 +0530

    reward change

--- Ring_Road/envs/ringroad.py
+++ Ring_Road/envs/ringroad.py
@@ -122,7 +122,7 @@ class RingRoad(gym.Env):
 
         # reward average velocity
         eta_2 = 4.
-        reward = self.state_extractor.get_average_vel() / 5
+        reward = self.state_extractor.get_average_vel()
         # punish accelerations (should lead to reduced stop-and-go waves)
         eta = 0.25  # 0.25
         mean_actions = eta * np.mean(np.abs(action))
