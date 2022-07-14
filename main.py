c564722c229a67bd45aeb6d6234648e30fda3a46
Vamsi995 <saivamsi.ds123@gmail.com>
2022-07-14 17:16:12 +0530

    changing warmup steps

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
         scripts.train_dqn.evaluate("/home/vamsi/Documents/GitHub/RingRoad-RL/Models/DQN/DQNTrainer_2022-03-03_19-28-07/DQNTrainer_ringroad-v1_f4621_00000_0_2022-03-03_19-28-08/checkpoint_000488/checkpoint-488")
 
     elif args.type == "eval_ppo":
-        scripts.train_ppo.evaluate("/home/vamsi/Documents/GitHub/RingRoad-RL/Models/PPO/SingleAgent/PPOTrainer_2022-07-13_22-22-39/PPOTrainer_ringroad-v1_344f2_00000_0_2022-07-13_22-22-39/checkpoint_000250/checkpoint-250")
+        scripts.train_ppo.evaluate("/home/vamsi/Documents/GitHub/RingRoad-RL/Models/PPO/SingleAgent/PPOTrainer_2022-07-14_12-10-22/PPOTrainer_ringroad-v1_d5e13_00000_0_2022-07-14_12-10-22/checkpoint_000250/checkpoint-250")
 
     elif args.type == "eval_idm":
         scripts.evaluate_idm.evaluate()
